f249302bf6377afda6bbdd49701155c810d35860
vivid <vividwwy@outlook.com>
2022-08-14 20:53:23 +0800

    aaa

---
+++ src/Two_one/Test_two.java
@@ -0,0 +1,9 @@
+package Two_one;
+
+public class Test_two {
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssssss");
+        System.out.println("sss");
+        System.out.println("slfdkajfklsadjfklsadjflksadjf;kasjdf");
+    }
+}
---
+++ src/one_one/Test1.java
@@ -0,0 +1,7 @@
+package one_one;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("sdkjfalkdfjs;lakdf");
+    }
+}
